772fd7ca2fe4500d8f37696a157abed3808939ab
tanahiro2010 <129029050+tanahiro2010@users.noreply.github.com>
2023-07-04 16:29:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,76 +19,25 @@ def nybox(title, main):
 
 
 # main program
-
-teki = random.randint(1, 3)  
-print(teki)
-tk.Tk().withdraw()
-x = simpledialog.askstring('じゃんけんgame', 'グーなら1、チョキなら2，パーなら3を入力してください')
-if teki == 1:
-    if x == 1:
-        tk.Tk().withdraw()
-        messagebox.showinfo("惜しい！あと一押し","あいこです")
-    
-    if x == 2:
-        tk.Tk().withdraw()
-        messagebox.showinfo("残念","あなたの負けです")
-
-    if x == 3:
-        tk.Tk().withdraw()
-        messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
-
-elif teki == 2:
-    if x == 1:
-        tk.Tk().withdraw()
-        messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
-    
-    elif x == 2:
-        tk.Tk().withdraw()
-        messagebox.showinfo("惜しい！あと一押し","あいこです")
-
-    else:
-        tk.Tk().withdraw()
-        messagebox.showinfo("残念", "残念、あなたの負けです")
-
-else:
-    if x == 1:
-        tk.Tk().withdraw()
-        messagebox.showinfo("残念", "残念、あなたの負けです")
-    
-    elif x == 2:
-        tk.Tk().withdraw()
-        messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
-
-    else:
-        tk.Tk().withdraw()
-        messagebox.showinfo("惜しい！あと一押し","あいこです")
-
-#2回目から
-vs = ""
-print(vs)
-_random_ = random.randint(1, 3)
-print(_random_)
-
-tk.Tk().withdraw()
-messagebox.showinfo("2回戦","第2回戦スタート‼")
-while vs != 0:
+while True:
+    teki = random.randint(1, 3)  
+    print(teki)
     tk.Tk().withdraw()
     x = simpledialog.askstring('じゃんけんgame', 'グーなら1、チョキなら2，パーなら3を入力してください')
-
-    if _random_ == 1:
+    if teki == 1:
         if x == 1:
             tk.Tk().withdraw()
             messagebox.showinfo("惜しい！あと一押し","あいこです")
-
-        elif x == 2:
+    
+        if x == 2:
             tk.Tk().withdraw()
-            messagebox.showinfo("残念", "残念、あなたの負けです")
- 
-        else:              
+            messagebox.showinfo("残念","あなたの負けです")
+
+        if x == 3:
             tk.Tk().withdraw()
             messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
 
-    if _random_ == 2:
+    elif teki == 2:
         if x == 1:
             tk.Tk().withdraw()
             messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
@@ -101,7 +50,7 @@ while vs != 0:
             tk.Tk().withdraw()
             messagebox.showinfo("残念", "残念、あなたの負けです")
 
-    elif _random_ == 3:
+    else:
         if x == 1:
             tk.Tk().withdraw()
             messagebox.showinfo("残念", "残念、あなたの負けです")
@@ -109,6 +58,62 @@ while vs != 0:
         elif x == 2:
             tk.Tk().withdraw()
             messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
+
+        else:
+            tk.Tk().withdraw()
+            messagebox.showinfo("惜しい！あと一押し","あいこです")
+
+    #2回目から
+    vs = ""
+    print(vs)
+    _random_ = random.randint(1, 3)
+    print(_random_)
+
+    tk.Tk().withdraw()
+    messagebox.showinfo("2回戦","第2回戦スタート‼")
+    while vs != 0:
+        tk.Tk().withdraw()
+        x = simpledialog.askstring('じゃんけんgame', 'グーなら1、チョキなら2，パーなら3を入力してください')
+
+        if _random_ == 1:
+            if x == 1:
+                tk.Tk().withdraw()
+                messagebox.showinfo("惜しい！あと一押し","あいこです")
+
+            elif x == 2:
+                tk.Tk().withdraw()
+                messagebox.showinfo("残念", "残念、あなたの負けです")
+ 
+            else:              
+                tk.Tk().withdraw()
+                messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
+
+        if _random_ == 2:
+            if x == 1:
+                tk.Tk().withdraw()
+                messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
+    
+            elif x == 2:
+                tk.Tk().withdraw()
+                messagebox.showinfo("惜しい！あと一押し","あいこです")
+
+            else:
+                tk.Tk().withdraw()
+                messagebox.showinfo("残念", "残念、あなたの負けです")
+
+        elif _random_ == 3:
+            if x == 1:
+                tk.Tk().withdraw()
+                messagebox.showinfo("残念", "残念、あなたの負けです")
+    
+            elif x == 2:
+                tk.Tk().withdraw()
+                messagebox.showinfo("おめでとう！","あなたは勝利しました！おめでとう")
+
+            else:
+                tk.Tk().withdraw()
+                messagebox.showinfo("惜しい！あと一押し","あいこです")
+
 
         else:
             tk.Tk().withdraw()
